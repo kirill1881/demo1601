7bfacdddecd0359d26c0f2e5d60c80ab719824ff
kirillsergeev <kirillovk317@gmail.com>
2023-01-03 01:41:55 +0300

    123

--- src/main/java/com/example/demo1601/controller/Main.java
+++ src/main/java/com/example/demo1601/controller/Main.java
@@ -2,9 +2,11 @@ package com.example.demo1601.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class Main {
     @GetMapping
     public String a(){
